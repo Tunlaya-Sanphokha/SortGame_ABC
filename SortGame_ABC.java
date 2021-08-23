79a45395658d3fd03b967eb338b5550ecb89f9b4
Natchanon-Walab <s6201012620074@email.kmutnb.ac.th>
2021-08-24 00:29:58 +0700

    Edit Board Game

--- SortGame_ABC.java
+++ SortGame_ABC.java
@@ -23,7 +23,6 @@ public class SortGame_ABC implements MouseListener{
   public static void main(String[] args){
     SortGame_ABC s = new SortGame_ABC();
     s.setup();
-
   }
 
   public void setup(){
@@ -31,74 +30,84 @@ public class SortGame_ABC implements MouseListener{
     DrawBoard draw = new DrawBoard();
     
     JLabel  l1 = new JLabel();
-    l1.setBounds(20,10,90,100);
+    l1.setBounds(45,25,90,100);
     l1.setFont(new Font("Paytone",Font.BOLD,90));
     l1.setText(BoardGame[0][0]);
 
     JLabel  l2 = new JLabel();
-    l2.setBounds(10,10,90,100);
+    l2.setBounds(195,25,90,100);
     l2.setFont(new Font("Paytone",Font.BOLD,90));
     l2.setText(BoardGame[0][1]);
 
     JLabel  l3 = new JLabel();
-    l3.setBounds(10,10,90,100);
+    l3.setBounds(345,25,90,100);
     l3.setFont(new Font("Paytone",Font.BOLD,90));
     l3.setText(BoardGame[0][2]);
 
     JLabel  l4 = new JLabel();
-    l4.setBounds(10,10,90,100);
+    l4.setBounds(495,25,90,100);
     l4.setFont(new Font("Paytone",Font.BOLD,90));
     l4.setText(BoardGame[0][3]);
 
     JLabel  l5 = new JLabel();
-    l5.setBounds(10,10,90,100);
+    l5.setBounds(45,175,90,100);
     l5.setFont(new Font("Paytone",Font.BOLD,90));
     l5.setText(BoardGame[1][0]);
 
     JLabel  l6 = new JLabel();
-    l6.setBounds(10,10,90,100);
+    l6.setBounds(195,175,90,100);
     l6.setFont(new Font("Paytone",Font.BOLD,90));
     l6.setText(BoardGame[1][1]);
 
     JLabel  l7 = new JLabel();
-    l7.setBounds(10,10,90,100);
+    l7.setBounds(345,175,90,100);
     l7.setFont(new Font("Paytone",Font.BOLD,90));
     l7.setText(BoardGame[1][2]);
 
     JLabel  l8 = new JLabel();
-    l8.setBounds(10,10,90,100);
+    l8.setBounds(495,175,90,100);
     l8.setFont(new Font("Paytone",Font.BOLD,90));
     l8.setText(BoardGame[1][3]); 
 
     JLabel  l9 = new JLabel();
-    l9.setBounds(10,10,90,100);
+    l9.setBounds(45,325,90,100);
     l9.setFont(new Font("Paytone",Font.BOLD,90));
     l9.setText(BoardGame[2][0]);
 
     JLabel  l10 = new JLabel();
-    l10.setBounds(10,10,90,100);
+    l10.setBounds(195,325,90,100);
     l10.setFont(new Font("Paytone",Font.BOLD,90));
     l10.setText(BoardGame[2][1]);
 
     JLabel  l11 = new JLabel();
-    l11.setBounds(10,10,90,100);
+    l11.setBounds(345,325,90,100);
     l11.setFont(new Font("Paytone",Font.BOLD,90));
     l11.setText(BoardGame[2][2]);
 
     JLabel  l12 = new JLabel();
-    l12.setBounds(10,10,90,100);
+    l12.setBounds(495,325,90,100);
     l12.setFont(new Font("Paytone",Font.BOLD,90));
     l12.setText(BoardGame[2][3]);
 
-
-    frame.add(l1); frame.add(l2);
+    frame.add(l1);
+    frame.add(l2);
+    frame.add(l3);
+    frame.add(l4);
+    frame.add(l5);
+    frame.add(l6);
+    frame.add(l7);
+    frame.add(l8);
+    frame.add(l9);
+    frame.add(l10);
+    frame.add(l11);
+    frame.add(l12);
     frame.add(draw);
+
     frame.setSize(618, 491);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setVisible(true);
     frame.setLayout(null);
     
-
   }
 
 @Override
@@ -141,13 +150,11 @@ public void mouseClicked(MouseEvent ev){
 @Override
 public void mousePressed(MouseEvent e) {
   
-  
 }
 
 @Override
 public void mouseReleased(MouseEvent e) {
  
-  
 }
 
 @Override
@@ -157,10 +164,6 @@ public void mouseEntered(MouseEvent e) {
 
 @Override
 public void mouseExited(MouseEvent e) {
- 
   
 }
-
-
-
 }
