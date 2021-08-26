80398c9c3dbeb72107231e389e331d77e2399b26
Natchanon-Walab <s6201012620074@email.kmutnb.ac.th>
2021-08-26 23:34:03 +0700

    Add color

--- SortGame_ABC.java
+++ SortGame_ABC.java
@@ -17,7 +17,7 @@ class DrawBoard extends Canvas{
           }
       }
 public class SortGame_ABC implements MouseListener{
-  public static String BoardGame[][] = {{"A","B","C","D"},{"E","F","G","H"},{"I","J","K"," "}};
+  public static String BoardGame[][] = {{"A","B","C","D"},{"E","F","G","H"},{"I","J"," ","K"}};
   public JFrame frame = new JFrame("SortGame ABC");
   
   public static void main(String[] args){
@@ -32,63 +32,64 @@ public class SortGame_ABC implements MouseListener{
     JLabel  l1 = new JLabel();
     l1.setBounds(45,25,90,100);
     l1.setFont(new Font("Paytone",Font.BOLD,90));
-    
+    l1.setForeground(Color.BLUE);
 
     JLabel  l2 = new JLabel();
     l2.setBounds(195,25,90,100);
     l2.setFont(new Font("Paytone",Font.BOLD,90));
+    l2.setForeground(Color.BLUE);
     
 
     JLabel  l3 = new JLabel();
     l3.setBounds(345,25,90,100);
     l3.setFont(new Font("Paytone",Font.BOLD,90));
-    
+    l3.setForeground(Color.BLUE);
 
     JLabel  l4 = new JLabel();
     l4.setBounds(495,25,90,100);
     l4.setFont(new Font("Paytone",Font.BOLD,90));
-    
+    l4.setForeground(Color.BLUE);
 
     JLabel  l5 = new JLabel();
     l5.setBounds(45,175,90,100);
     l5.setFont(new Font("Paytone",Font.BOLD,90));
-    
+    l5.setForeground(Color.BLUE);
 
     JLabel  l6 = new JLabel();
     l6.setBounds(195,175,90,100);
     l6.setFont(new Font("Paytone",Font.BOLD,90));
-    
+    l6.setForeground(Color.BLUE);
 
     JLabel  l7 = new JLabel();
     l7.setBounds(345,175,90,100);
     l7.setFont(new Font("Paytone",Font.BOLD,90));
-    
+    l7.setForeground(Color.BLUE);
 
     JLabel  l8 = new JLabel();
     l8.setBounds(495,175,90,100);
     l8.setFont(new Font("Paytone",Font.BOLD,90));
-    
+    l8.setForeground(Color.BLUE);
 
     JLabel  l9 = new JLabel();
     l9.setBounds(45,325,90,100);
     l9.setFont(new Font("Paytone",Font.BOLD,90));
-    
+    l9.setForeground(Color.BLUE);
 
     JLabel  l10 = new JLabel();
     l10.setBounds(195,325,90,100);
     l10.setFont(new Font("Paytone",Font.BOLD,90));
-    
+    l10.setForeground(Color.BLUE);
 
     JLabel  l11 = new JLabel();
     l11.setBounds(345,325,90,100);
     l11.setFont(new Font("Paytone",Font.BOLD,90));
-    
+    l11.setForeground(Color.BLUE);
 
     JLabel  l12 = new JLabel();
     l12.setBounds(495,325,90,100);
     l12.setFont(new Font("Paytone",Font.BOLD,90));
+    l12.setForeground(Color.BlUE);
     
-
     frame.add(l1);
     frame.add(l2);
     frame.add(l3);
@@ -118,8 +119,6 @@ public class SortGame_ABC implements MouseListener{
       l9.setText(BoardGame[2][0]);l10.setText(BoardGame[2][1]);l11.setText(BoardGame[2][2]);l12.setText(BoardGame[2][3]);
     }
 
-
-    
   }
 
 @Override
